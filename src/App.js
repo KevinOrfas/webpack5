446c8cecf5907a679405f03900d74010af784f60
Anastasios Orfanidis <anastasios@kopernio.com>
2021-05-29 14:38:33 +0100

    Set up the JS-React-Env with webpack 5

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Heading } from "./Heading.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading text={"JS ENV"} />
+    </div>
+  );
+}
+
+export default App;
